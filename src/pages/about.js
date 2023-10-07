e6305202a26acd5e93333feaf4891d35ac64def2
Arnt <arnt.lockert@gmail.com>
2023-10-07 18:11:26 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
     return (
